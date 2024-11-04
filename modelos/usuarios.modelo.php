0bde563410e91009519dc89c8b17b9444b15992e
johnnysuri-dev <johnnysuri-dev>
2024-11-04 15:12:12 -0400

    =actualuaciolizado once

--- modelos/usuarios.modelo.php
+++ modelos/usuarios.modelo.php
@@ -5,7 +5,7 @@ require_once "conexion.php";
 class ModeloUsuarios{
 
 
-		static public function mdlMostrarUsuarios($tabla, $item, $valor){
+	static public function mdlMostrarUsuarios($tabla, $item, $valor){
 
 		if($item != null){
 			$stmt = Conexion::conectar()->prepare("SELECT * FROM $tabla WHERE $item = :$item");
@@ -23,7 +23,7 @@ class ModeloUsuarios{
 	}
 // registro de usuario
 
-static public function mdlIngresarUsuario($tabla,$datos){
+	static public function mdlIngresarUsuario($tabla,$datos){
 
 		$stmt= Conexion::conectar()->prepare("INSERT INTO $tabla(nombre, usuario, password, perfil, foto) VALUES(:nombre, :usuario, :password, :perfil, :foto)");
 
@@ -40,8 +40,36 @@ static public function mdlIngresarUsuario($tabla,$datos){
 
 			$stmt->close();
 			$stmt =null;		
-		}
+	}
 
+// editar usuario
+	static public function mdlEditarUsuario($tabla, $datos){
+   
+        $stmt = Conexion::conectar()->prepare("UPDATE $tabla SET nombre = :nombre, password = :password, perfil = :perfil, foto = :foto WHERE usuario = :usuario");
+
+        $stmt -> bindParam(":nombre", $datos["nombre"], PDO::PARAM_STR);
+        $stmt -> bindParam(":password", $datos["password"], PDO::PARAM_STR);
+        $stmt -> bindParam(":perfil", $datos["perfil"], PDO::PARAM_STR);
+        $stmt -> bindParam(":foto", $datos["foto"], PDO::PARAM_STR);
+        $stmt -> bindParam(":usuario", $datos["usuario"], PDO::PARAM_STR);
+
+        if($stmt -> execute()){
+
+            return "ok";
+       
+        }else{
+
+            return "error";
+
+        }
+
+        $stmt -> close();
+
+        $stmt = null;
+
+    }
+
+	
 
 
 }
--- vistas/js/usuarios.js
+++ vistas/js/usuarios.js
@@ -46,3 +46,27 @@ $(".nuevaFoto").change(function(){
 
   	}
 })
+/*=============================================
+EDITAR USUARIO
+=============================================*/
+/*=====================EDITAR USUARIO==============*/
+$(document).on("click", ".btnEditarUsuario", function(){
+    var idUsuario = $(this).attr("idUsuario");
+    var datos = new FormData();
+    datos.append("idUsuario", idUsuario);
+    $.ajax({
+        url:"ajax/usuarios.ajax.php",
+        method: "POST",
+        data: datos,
+        cache: false,
+        contentType: false,
+        processData: false,
+        dataType: "json",
+        success: function(respuesta){       
+            $("#editarNombre").val(respuesta["nombre"]);
+            $("#editarUsuario").val(respuesta["usuario"]);
+            $("#editarPerfil").html(respuesta["perfil"]);
+
+        }
+    });
+})
